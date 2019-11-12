54fa556b6a5db372f35ba2547f55b206def0dedf
Noordeen Malango <noordinmalango@gmail.com>
2019-11-12 12:32:24 +0200

    change server using express

--- Server.js
+++ Server.js
@@ -1,65 +1,38 @@
-var http = require('http'); // 1 - Import Node.js core module
-
-var server = http.createServer(function (req, res) {   // 2 - creating server
-
-    if (req.url == '/') { //check the URL of the current request
-        
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
-        // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    
+//Import the necessary libraries/declare the necessary objects
+var express = require("express");
+var myParser = require("body-parser");
+var app = express();
+app.use(myParser.urlencoded({extended : true}));
+app.post("/sendmail", function(request, response) {
+    var nodemailer = require('nodemailer');
+    data = request.body; //This prints the JSON document received (if it is a JSON document)
+    //console.log(data.name);
+    var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'noordinmalango@gmail.com',
+        pass: '995751218'
     }
-    else if (req.url == "/sendmail") {
-        const { parse } = require('querystring');
-        var nodemailer = require('nodemailer');
-        if (req.method === 'POST') {
-            let body = '';
-            req.on('data', chunk => {
-                body += chunk.toString();
-            });
-            req.on('end', () => {
-                    var transporter = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: 'noordinmalango@gmail.com',
-                        pass: '995751218'
-                    }
-                    });
-                    var data = parse(body);
-                    var mailOptions = {
-                    from: 'noordinmalango@gmail.com',
-                    to: 'nmalango@medcol.mw',
-                    subject: 'Sending Email using Node.js',
-                    text: data
-                    };
-            
-                    transporter.sendMail(mailOptions, function(error, info){
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                    });
-                    res.end('ok');
-            });
-        }else
-        {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<html><body><p>This is SendMail Page.</p></body></html>');
-            res.end();
-        }
-        
+    });
+    var body = 'We have received your query our agents will act upon your query \n'+'The following are the details you sent us\n'+
+    'subject: '+data.subject+'\nMessage : '+data.message;
+    var mailOptions = {
+    from: 'noordinmalango@gmail.com',
+    to: data.email,
+    subject: 'Successfully received your query',
+    text: body
+    };
 
-    
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({'success':0}));
+    } else {
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({'success':1}));
     }
-    else
-        res.end('Invalid Request!');
-
-});
-
-server.listen(5000); //3 - listen for any incoming requests
-
-console.log('Node.js web server at port 5000 is running..')
+    });
+    
+}); 
+//Start the server and make it listen for connections on port 8080
+app.listen(5000);
